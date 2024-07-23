5fd8a877601a7a60887821fa76f0d6f9f31b82a2
Máté Brösztl <brosztl.mate@gmail.com>
2024-07-23 19:24:19 +0200

    Implement live features and integrate floating comments

--- middleware.ts
+++ middleware.ts
@@ -3,5 +3,9 @@ import { clerkMiddleware } from "@clerk/nextjs/server";
 export default clerkMiddleware();
 
 export const config = {
-  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+  matcher: [
+    "/((?!.*\\..*|_next|auth/sign-in|auth/sign-up).*)",
+    "/",
+    "/(api|trpc)(.*)",
+  ],
 };
